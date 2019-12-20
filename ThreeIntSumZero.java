24f5195e79afa7dfbeb5fcad0033a0c3ed926def
sandhyaCK <53344572+sandhyaCK@users.noreply.github.com>
2019-12-20 22:16:29 +0530

    [Sandhya]added

--- ThreeIntSumZero.java
+++
@@ -1,9 +0,0 @@
-/*#******************************************************************************************************************
-        # @purpose :Demonstrate Distance Logic.
-        # @file  :Distance.java
-        # @author :Sandhya
-   #*****************************************************************************************************************/
-package com.BridgeLabz.FunctionalProgram;
-
-public class ThreeIntSumZero {
-}
